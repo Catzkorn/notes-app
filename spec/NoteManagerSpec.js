f7ca8b313baf0ecc306623c978678029db7d5650
Louis Kirkham <louiskirkham@gmail.com>
2020-10-13 22:13:45 +0100

    sorry not sorry for the cheese

--- spec/NoteManagerSpec.js
+++
@@ -1,8 +0,0 @@
-
-
-console.log("adds a note to the notes list");
-let noteManager = new NoteManager();
-noteManager.addNote('This is the note message');
-displayTests(checkEqual(noteManager.getNotes().length, 1));
-
-
