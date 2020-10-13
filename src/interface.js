8489a78d275bba0471b643ccc61b7abace2b0b01
cristina <c.fujiname@gmail.com>
2020-10-13 16:08:54 +0100

    Add overlay to page

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,5 @@
 addNoteOnClick();
-
+off();
 var noteManager = new NoteManager();
 
 function addNoteOnClick() {
@@ -17,15 +17,17 @@ function displayNotes() {
   let notesList = document.getElementById("note-list");
   let notes = noteManager.getNotes();
   let html = ""
-  for (let i = 0; i < notes.length; i++) {
+  let notesCount = notes.length
+  for (let i = 0; i < notesCount; i++) {
     let note = notes[i];
     html += formatNote(note, i);
   }
   notesList.innerHTML = (html);
+  viewFullNoteOnClick(notesCount);
 }
 
 function formatNote(note, index) {
-  return `<div id='note-${index}'>` + abbreviateMessage(note.message) + " " + viewButton(index) + "<br>" + note.time + "</div><br>" 
+  return `<div id='note-${index}'>` + abbreviateMessage(note.message) + " " + generateViewButton(index) + "<br>" + note.time + "</div><br>" 
 }
 
 function abbreviateMessage(message) {
@@ -36,8 +38,23 @@ function abbreviateMessage(message) {
   }
 }
 
-function viewButton(index) {
+function generateViewButton(index) {
   return `<button type='button' id='button-${index}'>View</button>`
 }
 
+function viewFullNoteOnClick(notesCount) {
+  for (let i = 0; i < notesCount; i++ ) {
+    let viewButton = document.getElementById(`button-${i}`);
+    viewButton.addEventListener("click", function(clickEvent) {
+      on()
+    });  
+  };
+};
 
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
