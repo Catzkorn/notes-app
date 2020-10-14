fa3f54e210c4c29b2b82f405052966a117051ac4
cristina <c.fujiname@gmail.com>
2020-10-14 12:24:14 +0100

    Restyle form

--- src/Note.js
+++ src/Note.js
@@ -1,12 +1,7 @@
 class Note {
   constructor(message, time = new Date()) {
     this.message = message;
-    var day = time.getDate();
-    var month = time.getMonth();
-    var year = time.getFullYear();
-    var hour = time.getHours();
-    var minute = time.getMinutes();
-    this.time = `On ${day}/${month}/${year} at ${hour}:${minute} : ` ;
+    this.time = `On ${time.getDate()}/${time.getMonth()}/${time.getFullYear()} at ${time.getHours()}:${time.getMinutes()} : ` ;
   };
 
   getMessage() {
@@ -17,12 +12,4 @@ class Note {
     return this.time;
   };
 
-  // formatDate(date) {
-  //   var day = date.getDate();
-  //   var month = date.getMonth();
-  //   var year = date.getFullYear();
-  //   var hour = date.getHours();
-  //   var minute = date.getMinutes();
-  //   return `${day}, ${month} ${year} at ${hour}:${minute}` 
-  // }
 }
