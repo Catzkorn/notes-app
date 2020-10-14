135a5cea0cde9f24e748bf5f871421dbb3360161
Oliver Smith <olliesmith3@rocketmail.com>
2020-10-14 10:47:14 +0100

    Note creation (#11)
    * Update viewNotes to display a list of notes
    
    * Refactor displayNotes to have div with id
    
    * added abbreviate note function
    
    * added view button
    
    * Add overlay to page
    
    * Add message and close button to full note overlay
    
    * Refactored addClickListener
    
    Co-authored-by: Veronica Lee <veronicavlee@icloud.com>
    Co-authored-by: cristina <c.fujiname@gmail.com>
    Co-authored-by: Cristina Yuri Fujiname <67818167+cfujiname@users.noreply.github.com>

--- src/interface.js
+++ src/interface.js
@@ -1,24 +1,80 @@
 addNoteOnClick();
-
+off();
 var noteManager = new NoteManager();
 
-viewNotes();
+function addClickListener(elementId, clickFunction) {
+  let button = document.getElementById(elementId);
+  button.addEventListener("click", function(clickEvent) {
+    clickFunction(clickEvent)
+  })
+}
 
 function addNoteOnClick() {
-  var element = document.getElementById("submit-button");
-  element.addEventListener("click", function(clickEvent) {
+  addClickListener("submit-button", function(clickEvent) {
     clickEvent.preventDefault();
-    var message = document.getElementById('message').value;
-    noteManager.addNote(message);
-    viewNotes();
+    let message = document.getElementById('message');
+    noteManager.addNote(message.value);
+    displayNotes();
+    document.getElementById("new-note").reset();
   });
 };
 
-function viewNotes() {
-  var notesList = document.getElementById("note-list");
-  notesList.innerHTML = (noteManager.getNotes(noteManager)[0].message);
+function displayNotes() {
+  let notesList = document.getElementById("note-list");
+  let notes = noteManager.getNotes();
+  let html = ""
+  for (let i = 0; i < notes.length; i++) {
+    let note = notes[i];
+    html += formatNote(note, i);
+  }
+  notesList.innerHTML = (html);
+  viewFullNoteOnClick(notes);
+}
+
+function formatNote(note, index) {
+  return `<div id='note-${index}'>` + abbreviateMessage(note.message) + " " + generateViewButton(index) + "<br>" + note.time + "</div><br>" 
+}
+
+function abbreviateMessage(message) {
+  if (message.length > 20) {
+    return message.slice(0, 20) + "..."
+  } else {
+    return message
+  }
+}
+
+function generateViewButton(index) {
+  return `<button type='button' id='button-${index}'>View</button>`
+}
+
+function viewFullNoteOnClick(notes) {
+  for (let i = 0; i < notes.length; i++ ) {
+    addClickListener(`button-${i}`, function() {
+      let fullNote = document.getElementById('fullNote');
+      let note = notes[i];
+      fullNote.innerHTML = generateFullNote(note)
+      hideOverlayOnCloseClick();
+      on();
+    }); 
+  };
+};
+
+function generateFullNote(note) {
+  return note.time + '<br>' + note.message  + "<br> <button type='button' id='close'>Close</button>"
+}
+
+function hideOverlayOnCloseClick() {
+  addClickListener("close", function() {
+    off();
+  })
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
 }
 
 
-
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
 
